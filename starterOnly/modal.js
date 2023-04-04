6842fbc27b3ed9d3a41952b596e8425790e50bec
Julien Bergé <berge.jul@gmail.com>
2023-04-04 12:41:31 +0200

    Test Ok

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -9,15 +9,43 @@ function editNav() {
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
+const heroSec = document.querySelector(".hero-section");
+const submitBtn = document.querySelector(".btn-submit");
+const reservSec = document.querySelector(".reserv");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelectorAll(".close")
+console.log(submitBtn)
+console.log(heroSec)
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  console.log(screen.width);
+  //Permet  a la modal de prendre tout l'écran en version tablette ou mobile//
+  if ( screen.width < 1050 ){
+    heroSec.style.visibility = "hidden";
+    modalbg.style.display = "block";
+    heroSec.style.height = "0px";
+  }
+  else{
+    modalbg.style.display = "block";
+  }
 }
+
+//close Modal//
+closeBtn[0].addEventListener("click", closeModal);
+function closeModal() {
+    heroSec.style.visibility = "visible";
+    modalbg.style.display = "none";
+    heroSec.style.height = "auto";
+}
+
+//Confirm Message//
+submitBtn.onclick = () =>   console.log("confierm");
 
 
