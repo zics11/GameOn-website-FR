44e401975a04d8f32fd921901e5a0346eff8b15c
Julien Bergé <berge.jul@gmail.com>
2023-04-04 15:52:27 +0200

    Message de confirmation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,13 +10,13 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const heroSec = document.querySelector(".hero-section");
-const submitBtn = document.querySelector(".btn-submit");
+const submitBtn = document.querySelectorAll(".btn-submit");
 const reservSec = document.querySelector(".reserv");
+const confirmSec = document.querySelector(".confirm");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelectorAll(".close")
 console.log(submitBtn)
-console.log(heroSec)
 
 
 // launch modal event
@@ -39,13 +39,21 @@ function launchModal() {
 
 //close Modal//
 closeBtn[0].addEventListener("click", closeModal);
+submitBtn[1].addEventListener("click", closeModal)
+
 function closeModal() {
-    heroSec.style.visibility = "visible";
-    modalbg.style.display = "none";
-    heroSec.style.height = "auto";
+  heroSec.style.visibility = "visible";
+  modalbg.style.display = "none";
+  confirmSec.style.display = "none";
+  reservSec.style.display = "block";
+  heroSec.style.height = "auto";
 }
 
-//Confirm Message//
-submitBtn.onclick = () =>   console.log("confierm");
+//Cofirm message//
 
+submitBtn[0].addEventListener("click", cofirmMessage)
 
+function cofirmMessage() {
+  confirmSec.style.display = "block";
+  reservSec.style.display = "none";
+}
